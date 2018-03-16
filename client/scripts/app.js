1d33f186abccd7baf8a5a0fdc12e1edb88a030fc
Rongsiu Lim <rongsiu@gmail.com>
2018-03-16 14:28:28 -0700

    solo portion before pair

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,39 @@
-// YOUR CODE HERE:
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfm6.hackreactor.com/',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
+
+
+$.ajax({
+    // always use this url
+    url: 'http://parse.sfm6.hackreactor.com/',
+    type: 'GET',
+    data: 'order=-createdAt',
+    contentType: 'application/json',
+    success: function (data) {
+        _.each(data.results, function(item){
+            $('#messages').append('<li>'+item.text+'</li>');
+        });
+    },
+    error: function (data) {
+        console.error('chatterbox: Failed to send message');
+    }
+});
+
+
+//var access_token = " "
+//$.get()
+
+
