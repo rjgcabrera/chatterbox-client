0e4746d0ce96d2afdf9cae14e01ed18af6cef946
rjgcabrera <rjgcengr@gmail.com>
2018-03-17 14:44:03 -0700

    Implement friend list, bolded friends' messages, and room rendering

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,7 @@
 var app = {
   //set: new Set(),
   friends: [],
+  rooms: [],
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
 };
 
@@ -11,6 +12,17 @@ app.init = function() {
     // add other methods
     $('#send').submit(app.handleSubmit);
     $('#chats').on('click', '.username', app.handleUsernameClick);
+    $('#roomSelect').change(function(){
+       var selectedRoom = $('#roomSelect option:selected').text();
+       console.log(selectedRoom);
+    //  if (!$('.message').hasClass(selectedRoom)) {
+
+    //  }
+       //$(`.message:not(.${selectedRoom})`).toggle();
+       $('.message').show();
+       $(`.message:not(.${selectedRoom})`).toggle();
+       //$(`.${selectedRoom}`).toggle();
+    });
     //$('.username').on('click', app.handleUsernameClick);
     
 };
@@ -42,7 +54,7 @@ app.fetch = function() {
         data: 'order=-createdAt',
         datatype: 'json',
         contentType: 'application/json',
-        success: function (data) {
+        success: function(data) {
             console.log(data);
              _.each(data.results, function(item) {
                  app.renderMessage(item);
@@ -64,41 +76,68 @@ app.renderMessage = function(item) {
     // var node = document.createElement('div')
   var $message = $('<div></div>');
   $message.addClass('message');
-  //$message.innerHTML = item.text;
+  //if statement
+  var room = item.roomname;
+  if(!room) {
+      room = "undefined";
+  }
+  $message.addClass(room);
   $message.append('<p class="username">' + _.escape(item.username) + '</p>');
   $message.append('<p>' + _.escape(item.text) + '</p>');
   $message.append('<p>' + _.escape(item.createdAt) + '</p>');
-//     $message.append('<p>' + item.username + '</p>');
-//   $message.append('<p>' + item.text + '</p>');
-//   $message.append('<p>' + item.creaedAt + '</p>');
+  app.renderRoom(room);
   $('#chats').append($message);
+
 };
 
 app.renderRoom = function(room) {
-    var $room = $('<div></div>');
-    $('#roomSelect').append($room);
+    if (!app.rooms.includes(room)) {
+      var $room = $('<option></option>');
+      app.rooms.push(room);
+      $room.append(room);
+      $('#roomSelect').append($room);
+    }
 };
 
-app.handleSubmit = function() {
+app.handleSubmit = function(event) {
     var message = {};
-    message.text =  $("message").val();
+    message.text =  $("#message").val();
     message.username = window.location.search.substring(10);
-    message.roomname = $("chatroom").val()
-    app.send(message)
+    message.roomname = $("#chatroom").val();
+    app.send(message);
+    $("#message").val('');
+    $("#chatroom").val('');
+    event.preventDefault();
 };
 
 app.handleUsernameClick = (event) => {
     if(!app.friends.includes($(event.target).text())) {
-    app.friends.push($(event.target).text());
-    var $friend = $('<div></div>');
-    console.log('yooo');
-    $friend.append($(event.target).text());
-    $('#friends').append($friend);
+        app.friends.push($(event.target).text());
+        var $friend = $('<div></div>');
+        console.log('yooo');
+        $friend.append($(event.target).text());
+        $('#friends').append($friend);
     }
+    // for (var i = 0; i < app.friends.length; i++) {
+    //     if($(".message").html().includes(app.friends[i])) {
+    //         this.addClass('isFriend')
+    //         $(".isFriend").css('font-weight', 'bold')
+    //     }
+    // }
+    var $messages = Array.from($('.message'));
+    for (var i = 0; i < $messages.length; i++) {
+        for (var j = 0; j < app.friends.length; j++) {
+            if ($messages[i].innerHTML.includes(`<p class="username">${app.friends[j]}</p>`)) {
+                $messages[i].style.fontWeight = 'bold';
+            }
+        }
+    }
+
 };
 
 $(document).ready(function() {
     app.init();
+    //setInterval(app.init, 3000);
     // $('.username').on(click, function() {app.set.add($(this).text());});
 });
 
@@ -108,5 +147,3 @@ $(document).ready(function() {
 
 //var access_token = " "
 //$.get()
-
-
